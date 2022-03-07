f479fb7b20efb9fa959dd23dbfd38ce5f07eef7e
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-08 02:20:18 +0600

    condition use

---
+++ fundamental.js
@@ -0,0 +1,12 @@
+const a=10
+const b=20
+const c=50
+
+if(a>b && a>c){
+    console.log("a is large number")
+}
+else if(b>a && b>c){
+    console.log("b is large number")
+}else{
+    console.log("c is large number")
+}
