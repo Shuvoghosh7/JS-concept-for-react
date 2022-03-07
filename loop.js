a475005b1054212756f767c9ab94504c72091085
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-08 03:04:50 +0600

    for & while loop

---
+++ loop.js
@@ -0,0 +1,16 @@
+for(let i=0;i<10;i++){
+    console.log(i)
+}
+const numbers=[10,20,30,40]
+for(let i=0;i<number.length;i++){
+    console.log(numbers[i])
+}
+for(const number of numbers){
+    console.log(number)
+}
+/* // while loop
+let i=0
+while(i<10){
+    console.log(i)
+    i++
+} */
