10077c5ab70581190ab42d78af1d6016c5b69f8a
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-08 03:21:48 +0600

    object

---
+++ object.js
@@ -0,0 +1,12 @@
+const student = {
+    name:'shuvo',
+    age:24,
+    address:['road-11','house-12','sector-11'],
+    subject:{
+        CSE:['cse-101','cse-202'],
+        math:'math-202'
+    }
+}
+console.log(student.address[0])
+console.log(student['name'])
+console.log(student.subject.CSE)
